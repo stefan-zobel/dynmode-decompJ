60a2e6c74167f60dc5180c4af99bb6c9c5af3043
stefan-zobel <stefan-zobel@users.noreply.github.com>
2021-04-08 20:37:28 +0200

    additional examples

---
+++ src/test/java/test/math/dmd/RelativeError.java
@@ -0,0 +1,36 @@
+package test.math.dmd;
+
+import net.jamu.matrix.Matrices;
+import net.jamu.matrix.MatrixD;
+
+class RelativeError {
+
+    static MatrixD compute(MatrixD expected, MatrixD actual) {
+        MatrixD diff = expected.minus(actual);
+        MatrixD avgRelErrorPerColumn = Matrices.createD(1, diff.numColumns());
+        for (int col = 0; col < diff.numColumns(); ++col) {
+            double sumErrPerColumn = 0.0;
+            for (int row = 0; row < diff.numRows(); ++row) {
+                double exp = Math.abs(expected.get(row, col));
+                double delta = Math.abs(diff.get(row, col));
+                double rel = delta / exp;
+                sumErrPerColumn += rel;
+            }
+            double avgErr = sumErrPerColumn / diff.numRows();
+            avgRelErrorPerColumn.set(0, col, avgErr);
+        }
+        return avgRelErrorPerColumn;
+    }
+
+    static double avgRelErrorOverall(MatrixD avgRelErrorPerColumn) {
+        double avgErr = 0.0;
+        for (int i = 0; i < avgRelErrorPerColumn.numColumns(); ++i) {
+            avgErr += avgRelErrorPerColumn.get(0, i);
+        }
+        return avgErr / avgRelErrorPerColumn.numColumns();
+    }
+
+    private RelativeError() {
+        throw new AssertionError();
+    }
+}
